656a4f934fbe32991226358ea7a6a049a909f20b
Pythonn-Developer <87891464+Pythonn-Developer@users.noreply.github.com>
2023-07-19 18:04:15 +0530

    Both countdown working

--- static/js/next_day_countdown.js
+++ static/js/next_day_countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 20, 2023 00:00:25").getTime();
+var countDownDate1 = new Date("July 20, 2023 00:00:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -7,17 +7,17 @@ var x = setInterval(function() {
   var now = new Date().getTime();
     
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  var distance1 = countDownDate1 - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours1 = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds1 = Math.floor((distance1 % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("countdown_for_next_day").innerHTML = hours + " Hours " + minutes + " Minutes " + 
-  seconds + " Seconds"; 
+  document.getElementById("countdown_for_next_day").innerHTML = hours1 + " Hours " + minutes1 + " Minutes " + 
+  seconds1 + " Seconds"; 
     
   // If the count down is over, write some text 
   if (distance < 0) {
